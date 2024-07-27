0755b417c2fe6a0db7dc305b66b4240b4b71f2fc
Jack Meyer <jaakmyr@gmail.com>
2024-07-27 13:22:20 -0700

    created new json file, got everything working like befre, doesn't redo on refresh but that's intentional, stores localstorage data

--- js/game.js
+++ js/game.js
@@ -1,10 +1,41 @@
+function submitForm(button){
+    alert(button)
+}
 
+function randomNumber(max){
+    return Math.floor(Math.random() * max)
+}
 
-const left = document.querySelector('#left')
-const right = document.querySelector('#right')
+async function getData(){
+    try{
+        //get data and populate localStorage, if empty
+        if(!localStorage.getItem('top')){
+            const result = await fetch('mapData.json')
+            const data = await result.json()
+            localStorage.setItem('top', JSON.stringify(data[randomNumber(1000)]))
+            localStorage.setItem('bottom', JSON.stringify(data[randomNumber(1000)]))
+        }
+        
+        populateDataField('#top', JSON.parse(localStorage.getItem('top')))
+        populateDataField('#bottom', JSON.parse(localStorage.getItem('bottom')))
+    } catch(e){
+        console.error(e)
+    }
+}  
 
-//left.querySelector('img').src = 
-// const beatmapinfo = getBeatMap(1)
-// alert(beatmapinfo)
+function populateDataField(parentId, data){
+    const parent = document.querySelector(parentId)
+    const dataFields = parent.querySelectorAll('[data-field]')
+    for(let i = 0; i < dataFields.length; i++){
+        //this will be mapper or cover etc
+        const attribute = dataFields[i].getAttribute('data-field') 
 
-getBeatMap(0)
+        if(attribute === 'cover') dataFields[i].src = data['cover']
+        else if(attribute === 'playCount') dataFields[i].textContent = data['playcount'].toLocaleString();
+        else if(attribute === 'title')dataFields[i].textContent = data['title']
+        else if(attribute === 'mapper')dataFields[i].textContent = data['creator']
+        else if(attribute === 'audio')dataFields[i].textContent = data['audio']
+    }
+}
+
+getData()
