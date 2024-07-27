10feef2664e84df0cfdb3c630943395d1b465715
Jack Meyer <jaakmyr@gmail.com>
2024-07-27 00:07:50 -0700

    okay ill do this tmr, need to add the json file wiwth data

---
+++ js/game.js
@@ -0,0 +1,10 @@
+
+
+const left = document.querySelector('#left')
+const right = document.querySelector('#right')
+
+//left.querySelector('img').src = 
+// const beatmapinfo = getBeatMap(1)
+// alert(beatmapinfo)
+
+getBeatMap(0)
